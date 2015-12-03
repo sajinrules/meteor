fb7f22874e75f8e877e9f46d8db400228d736f5d
sajin <sajinrules@gmail.com>
2015-12-03 19:40:37 +0530

    initial commit

---
+++ client/templates/task/tasks.js
@@ -0,0 +1,8 @@
+Tasks = new Mongo.Collection('tasks');
+Template.tasks.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+  });
